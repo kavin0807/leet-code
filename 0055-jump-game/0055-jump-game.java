b14307f0585c8c09293d87782ffc6b00402b2278
M.Kavin Prabhu <149070761+kavin0807@users.noreply.github.com>
2025-01-08 21:33:37 +0530

    Time: 2 ms (79.43%), Space: 45.6 MB (76.39%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length <= 1) return true;
+        int max = nums[0]; // max posible rechable index
+        int i = 0;
+        while(i <= max){
+            max = Math.max(nums[i] + i++, max);
+            if(max >= nums.length -1) return true;
+        }
+        return false;
+    }
+}
